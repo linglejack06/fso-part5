8054e7f01f313a31005bdcc313584cdec19b8420
Jack Lingle <captainjack0818@gmail.com>
2023-06-29 17:15:33 -0400

    add Togglable component to extract visibility

---
+++ note-frontend/src/components/Togglable.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const Togglable = ({ buttonLabel, children }) => {
+  const [visible, setVisible] = useState('');
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  }
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {children}
+        <button onClick={toggleVisibility}>Cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default Togglable;

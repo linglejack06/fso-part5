a894d2bbaac5994096be3220405a2d776dde8984
Jack Lingle <captainjack0818@gmail.com>
2023-06-30 18:35:05 -0400

    finish note component testing

--- note-frontend/src/components/Note.test.js
+++ note-frontend/src/components/Note.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Note from './Note';
 
 test('renders content', () => {
@@ -14,4 +15,18 @@ test('renders content', () => {
   expect(div).toHaveTextContent(
     'test',
   );
+});
+test('clicking button causes event handler to run', async () => {
+  const note = {
+    content: 'test',
+    important: false,
+  };
+  const mockHandler = jest.fn();
+
+  render(<Note note={note} toggleImportance={mockHandler} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText('make important');
+  await user.click(button);
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });

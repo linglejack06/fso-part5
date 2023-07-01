d4b55004ff4e943f410668231d21aa6154e0fe37
Jack Lingle <captainjack0818@gmail.com>
2023-07-01 10:29:50 -0400

    update eslint for jest

--- blog-frontend/.eslintrc.cjs
+++ blog-frontend/.eslintrc.cjs
@@ -1,10 +1,18 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, "jest/globals": true, },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+  ],
+  overrides: [
+    {
+      files: ["./src/test/**"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      rules: { "jest/prefer-expect-assertions": "off" }
+    }
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },

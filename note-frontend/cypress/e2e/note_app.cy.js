725684169ba5ad40782392fd1de8da834685b186
Jack Lingle <captainjack0818@gmail.com>
2023-07-02 08:34:05 -0400

    update e2e test package

--- note-frontend/cypress/e2e/note_app.cy.js
+++ note-frontend/cypress/e2e/note_app.cy.js
@@ -1,5 +1,7 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  it('front page can be opened', () => {
+    cy.visit('http://localhost:3001');
+    cy.contains('Notes');
+    cy.contains('Note app, Department of Computer Science, University of Helsinski 2023')
+  });
+});

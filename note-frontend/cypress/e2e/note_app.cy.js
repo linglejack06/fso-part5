abe638ae52427afb06cc8d912d69fef161debaa0
Jack Lingle <captainjack0818@gmail.com>
2023-07-02 17:19:45 -0400

    update test to reset db after every test

--- note-frontend/cypress/e2e/note_app.cy.js
+++ note-frontend/cypress/e2e/note_app.cy.js
@@ -1,5 +1,12 @@
 describe('Note app', () => {
   beforeEach(() => {
+    cy.request('POST', 'http:localhost:3001/api/testing/reset');
+    const user = {
+      name: 'Jack Lingle',
+      username: 'jling',
+      password: 'password',
+    };
+    cy.request('POST', 'http:localhost:3001/api/users/', user);
     cy.visit('http://localhost:3001');
   });
   it('front page can be opened', () => {
@@ -14,7 +21,7 @@ describe('Note app', () => {
     cy.get('#username').type('jling');
     cy.get('#password').type('password');
     cy.contains('Login to Notes').click();
-    cy.contains('Logged In:');
+    cy.contains('Logged In: Jack Lingle');
   });
   describe('When logged in', () => {
     beforeEach(() => {

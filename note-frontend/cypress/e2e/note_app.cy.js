f47a775230906ec98b0a069c653b49d519587619
Jack Lingle <captainjack0818@gmail.com>
2023-07-02 08:47:04 -0400

    add more e2e tests for login form

--- note-frontend/cypress/e2e/note_app.cy.js
+++ note-frontend/cypress/e2e/note_app.cy.js
@@ -1,7 +1,19 @@
-describe('template spec', () => {
-  it('front page can be opened', () => {
+describe('Note app', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3001');
+  });
+  it('front page can be opened', () => {
     cy.contains('Notes');
     cy.contains('Note app, Department of Computer Science, University of Helsinski 2023');
+  });
+  it('login form can be opened', () => {
+    cy.contains('Login').click();
+  });
+  it('logs in user', () => {
+    cy.contains('Login').click();
+    cy.get('#username').type('jling');
+    cy.get('#password').type('password');
+    cy.contains('Login to Notes').click();
+    cy.contains('Logged In:');
   });
 });
